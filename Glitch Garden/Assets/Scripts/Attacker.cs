5ca9bfb7bf58b18322f10be7d2618545bd0c5e38
guicustodio <guiilherme_custodiio@hotmail.com>
2021-08-17 14:54:57 -0300

    Fixed Score Layer

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +8,40 @@ public class Attacker : MonoBehaviour
     [Range (0f, 4f)]
     [SerializeField] float currentSpeed = 0.1f;
 
+    GameObject currentTarget;
+
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed) {
 
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if(!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
